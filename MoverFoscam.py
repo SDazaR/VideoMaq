48f2128d0eea5d7eedaf590013b70608a09b83d5
SDazaR <sdrpagcfcgr96@gmail.com>
2019-03-15 23:19:13 -0500

    Rehacer script
    Se rehizo el script teniendo en cuenta el nuevo script Configuraciones con las clases Maquina y Cam

--- MoverFoscam.py
+++ MoverFoscam.py
@@ -1,12 +1,12 @@
 import requests
 import time
+import Configuraciones as conf
 
-def LlevarA(PresetPoint):
+def LlevarA(PresetPoint,Camara):
+    cam=conf.Cam(Camara)
     try:
-        params = (('cmd', 'ptzGotoPresetPoint'),('name', PresetPoint),('usr', 'maya'),('pwd', 'maya'),)
-
-        response = requests.get('http://192.168.1.71:88//cgi-bin/CGIProxy.fcgi', params=params)
-
+        params = (('cmd', 'ptzGotoPresetPoint'),('name', PresetPoint),('usr', cam.usr),('pwd', cam.pwd),)
+        response = requests.get('http://%s/cgi-bin/CGIProxy.fcgi'%cam.fuente, params=params)
         time.sleep(1)
     except:
         print "No existe el Preset Point especificado"
