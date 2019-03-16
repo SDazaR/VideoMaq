2f919a2fc2935b4aa070dbad89524eb56318a27d
SDazaR <sdrpagcfcgr96@gmail.com>
2019-03-15 23:17:42 -0500

    Rehacer script
    Se rehizo el script teniendo en cuenta el nuevo script Configuraciones con las clases Maquina y Cam

--- EncontrarGeometria.py
+++ EncontrarGeometria.py
@@ -1,12 +1,8 @@
 import cv2
 import numpy as np
+import Configuraciones as conf
 
 def Circulos(mask,camara):
-    if camara=='Foscam':
-        circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT,1,20,
-                            param1=180,param2=35,minRadius=0,maxRadius=0)
-    else:
-        circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT,1,20,
-                            param1=200,param2=40,minRadius=0,maxRadius=0)
+    cam=conf.Cam(camara)
+    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT,1,20,param1=cam.par1,param2=cam.par2,minRadius=0,maxRadius=0)
     return circles
-
